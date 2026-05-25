4e23ba3b53462ee346e439caddc7e35650554ab5
TheASDM <thespectrumdm@gmail.com>
2026-05-25 16:10:16 -0500

    fix: align wiki list markers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'vallombrosa-pwa-v6';
+const CACHE_VERSION = 'vallombrosa-pwa-v7';
 const PRECACHE = `${CACHE_VERSION}-precache`;
 const PAGES = `${CACHE_VERSION}-pages`;
 const ASSETS = `${CACHE_VERSION}-assets`;
