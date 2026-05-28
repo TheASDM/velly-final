7e251e5447f3ede37fe0d864398a8886cb4f1a63
TheASDM <thespectrumdm@gmail.com>
2026-05-28 01:58:57 -0500

    Fix Enzo menu stacking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'foglight-pwa-v29';
+const CACHE_VERSION = 'foglight-pwa-v30';
 const PRECACHE = `${CACHE_VERSION}-precache`;
 const PAGES = `${CACHE_VERSION}-pages`;
 const ASSETS = `${CACHE_VERSION}-assets`;
