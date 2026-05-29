7029b68cf05095166a89fb77499f373a808f9e30
TheASDM <thespectrumdm@gmail.com>
2026-05-29 02:22:44 -0500

    Hide floating Enzo pill

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'foglight-pwa-v35';
+const CACHE_VERSION = 'foglight-pwa-v36';
 const PRECACHE = `${CACHE_VERSION}-precache`;
 const PAGES = `${CACHE_VERSION}-pages`;
 const ASSETS = `${CACHE_VERSION}-assets`;
